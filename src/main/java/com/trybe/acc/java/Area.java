b140985cee9c5af0aba85cf2d67d2b3d4a5606fd
Marcos Albuquerque <desenvolvedorwebfrontend@gmail.com>
2022-07-28 13:17:46 -0300

    Initial commit

--- src/main/java/com/trybe/acc/java/Area.java
+++ src/main/java/com/trybe/acc/java/Area.java
@@ -1,21 +1,29 @@
 package com.trybe.acc.java;
 
+import java.lang.Math;
 import java.util.Locale;
 import java.util.Scanner;
 
+/** Calcula area do quadrado. */
 public class Area {
-   public static void main(String[] args) {
-    
-      // Ler a entrada de um número pelo console
-      Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
-      System.out.println("Digite a medida do lado do quadrado:");
-      double lado = scan.nextDouble();
-      
-      // Seu código aqui
+  /** Calcula area do quadrado. */
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+    StringBuilder stringBuilder = new StringBuilder();
 
+    System.out.println("Digite a medida do lado do quadrado:");
+    double lado = scan.nextDouble();
+    double area = Math.pow(lado, 2);
 
-      // Imprime a saída no console. OBS: descomente a próxima linha
-      //System.out.println("A area do quadrado de lado "+ lado + " é igual a " + area);
-   }
+    scan.close();
+
+    StringBuilder frase = stringBuilder
+                                        .append("A area do quadrado de lado ")
+                                        .append(lado)
+                                        .append(" é igual a ")
+                                        .append(area);
+        
+    System.out.println(frase);
+  }
 }
